ee63ddb15c855941ed1742e04a7bdce84263854c
Anvi Alex <epononanvialex@gmail.com>
2021-04-09 22:21:37 +0300

    Fix linter errors

--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -3,18 +3,17 @@ require 'nokogiri'
 require 'httparty'
 
 describe Scraper do
-
   describe '#total_review_num' do
     it 'returns an integer which represents the total reviews of the App.' do
       temp = Scraper.new
       k = temp.total_review_num
-      expect(k.is_a? Integer).to eql(true)
+      expect(k.is_a?(Integer)).to eql(true)
     end
 
     it 'returns an integer which represents the total reviews of the App.' do
       temp = Scraper.new
       k = temp.total_review_num
-      expect(k.is_a? String).to eql(false)
+      expect(k.is_a?(String)).to eql(false)
     end
   end
 
@@ -22,26 +21,26 @@ describe Scraper do
     it 'returns a float number representing the average score of appreciation of Revolut' do
       temp = Scraper.new
       k = temp.score
-      expect(k.is_a? Float).to eql(true)
+      expect(k.is_a?(Float)).to eql(true)
     end
-    
+
     it 'returns a float number representing the average score of appreciation of Revolut' do
       temp = Scraper.new
       k = temp.score
-      expect(k.is_a? String).to eql(false)
+      expect(k.is_a?(String)).to eql(false)
     end
   end
 
   describe '#review_resume' do
-    it 'return a hash with title, content, score and date info'do
+    it 'return a hash with title, content, score and date info' do
       temp = Scraper.new
       k = temp.review_resume
-      expect(k.has_key?(:title) && k.has_key?( :content) && k.has_key?(:score) && k.has_key?(:date) ).to eql(true)
+      expect(k.key?(:title) && k.key?(:content) && k.key?(:score) && k.key?(:date)).to eql(true)
     end
-    it 'return 4 elements key-value pairs'do
+    it 'return 4 elements key-value pairs' do
       temp = Scraper.new
       k = temp.review_resume
-      expect(k.length ).to eql(4)
+      expect(k.length).to eql(4)
     end
   end
 
