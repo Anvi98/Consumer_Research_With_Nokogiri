3e369ea0a27b658689eaaf84d294f8152ea9faf9
Anvi Alex <epononanvialex@gmail.com>
2021-04-09 22:40:28 +0300

    Fix linter issues

--- lib/core.rb
+++ lib/core.rb
@@ -19,9 +19,9 @@ class Scraper
   def review_resume
     @last_review_wrapper = @parsed_html.css('div.review-content')[0]
     @last_review = {
-      title: @last_review_wrapper.css('a').text.split(' ').slice(0, 3).push('..').join(' '),
-      content: @last_review_wrapper.css('p').text.split(' ').join(' '),
-      score: @last_review_wrapper.css('img').attr('alt').text.split(':')[0].split(' ')[0].to_i,
+      title: @last_review_wrapper.css('a').text.split.slice(0, 3).push('..').join(' '),
+      content: @last_review_wrapper.css('p').text.split.join(' '),
+      score: @last_review_wrapper.css('img').attr('alt').text.split(':')[0].split[0].to_i,
       date: @last_review_wrapper.css('div.review-content-header__dates').children[1].children.text.split('"')[3].split('T')[0]
     }
     @last_review
@@ -40,15 +40,15 @@ class Scraper
 
     while @page <= 20
       @review_list.each do
-        @reviews = @review_list.select { |img| img.attr('alt').split(':')[0].split(' ')[0].to_i }
+        @reviews = @review_list.select { |img| img.attr('alt').split(':')[0].split[0].to_i }
         @count_reviews = @reviews
       end
       @page += 1
     end
 
-    @count_reviews.each { |review| @excel += 1 if review.attr('alt').split(':')[0].split(' ')[0].to_i == 5 }
-    @count_reviews.each { |review| @good += 1 if review.attr('alt').split(':')[0].split(' ')[0].to_i == 4 }
-    @count_reviews.each { |review| @bad += 1 if review.attr('alt').split(':')[0].split(' ')[0].to_i <= 3 }
+    @count_reviews.each { |review| @excel += 1 if review.attr('alt').split(':')[0].split[0].to_i == 5 }
+    @count_reviews.each { |review| @good += 1 if review.attr('alt').split(':')[0].split[0].to_i == 4 }
+    @count_reviews.each { |review| @bad += 1 if review.attr('alt').split(':')[0].split[0].to_i <= 3 }
     @render = [@excel, @good, @bad]
     p @render.length
     @render
